8e2b02f2986797719ecf2cbf2e084f87bdc31a34
Eric <yalmancierkan1@gmail.com>
2023-01-12 11:28:21 +0300

    first commit

---
+++ flight/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from .models import Flight, Reservation, Passenger
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "flight_number",
+            "operation_airlines",
+            "departure_city",
+            "arrival_city",
+            "date_of_departure",
+            "etd"
+        )
+
+class PassengerSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Passenger
+        fields = "__all__"
+
+class ReservationSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    flight = serializers.StringRelatedField()
+    passenger = PassengerSerializer(many=True)
+    flight_id = serializers.IntegerField()
+    class Meta:
+        model = Reservation
+        fields = (
+            "id",
+            "flight",
+            "flight_id",
+            "user",
+            "passenger"
+        )
+    def create(self, validated_data):
+        passenger_data = validated_data.pop("passenger")
+        validated_data["user_id"] = self.context["request"].user.id # istek atan user'ın id'si
+        reservation = Reservation.objects.create(**validated_data)
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+
+        reservation.save()
+        return reservation
+
+class StaffFlightSerializer(serializers.ModelSerializer):
+    reservation = ReservationSerializer(many = True, read_only=True)
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "flight_number",
+            "operation_airlines",
+            "departure_city",
+            "arrival_city",
+            "date_of_departure",
+            "etd",
+            "reservation"
+        )
